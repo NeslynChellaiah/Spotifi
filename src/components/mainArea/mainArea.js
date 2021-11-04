964dc7e9275a6b48f783eecab3d0d5227af6a3c6
Neslyn <neslynchellaiah@gmail.com>
2021-11-04 13:08:25 +0530

    Added files for screens

--- src/components/mainArea/mainArea.js
+++ src/components/mainArea/mainArea.js
@@ -1,9 +1,23 @@
 import './mainArea.css';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
+import Home from './home/home';
 
 const MainArea = () => {
     return (
         <div className="main-area">
-
+            <div className="header">
+                <div className="header-section1">
+                    <button className="left-arrow">
+                        <AiOutlineLeft size="1.5em"/>
+                    </button>
+                    <button className="right-arrow">
+                        <AiOutlineRight size="1.5em"/>
+                    </button>
+                </div>
+            </div>
+            <div className="container">
+                <Home />
+            </div>
         </div>
     )
 }
